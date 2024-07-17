a996c6267d438e9517374cc69b8bd7d7e65953e3
roux-g <roux-guillaume@outlook.com>
2024-07-17 19:52:02 +0200

    Quartz sync: Jul 17, 2024, 7:52 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,9 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.MobileOnly(Component.Explorer())
+  ],
   footer: Component.Footer(),
   // footer: Component.Footer({
   //   links: {
